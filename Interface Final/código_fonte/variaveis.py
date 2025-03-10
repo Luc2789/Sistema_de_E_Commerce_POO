58f665470c9d2f3498a2ecccbd0aece13f269407
Wesley Silva <147096961+wdevsley@users.noreply.github.com>
2025-03-09 23:55:24 -0300

    Interface Final

---
+++ Interface Final/código_fonte/variaveis.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent
+FILES_DIR = ROOT_DIR / 'files'
+bleach = FILES_DIR / 'ble.webp'
+naruto = FILES_DIR / 'naruto-imagem.webp'
+one= FILES_DIR / 'one.webp'
+de = FILES_DIR / 'demon-image.webp'
+no = FILES_DIR / 'de.webp'
+at = FILES_DIR / 'atack-imagem.webp'
